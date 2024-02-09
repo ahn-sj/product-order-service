0a38475476e1c1f029456871570784360bb4f90e
ahn-sj <jaesa5221@gmail.com>
2024-02-09 19:44:30 +0900

    feat: 3-2 스프링부트 테스트로 전환하기

--- src/test/java/org/jae/productorderservice/product/ProductServiceTest.java
+++ src/test/java/org/jae/productorderservice/product/ProductServiceTest.java
@@ -1,57 +1,34 @@
 package org.jae.productorderservice.product;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
-    private ProductPort productPort;
-
-    @BeforeEach
-    void setUp() {
-        productPort = Mockito.mock(ProductPort.class);
-        productService = new ProductService(productPort);
-    }
 
     @Test
     @DisplayName("상품 수정")
     void 상품_수정() {
-
         // given:
+        productService.addProduct(ProductSteps.상품등록요청_생성());
         final Long productId = 1L;
-        final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         // when:
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
         productService.updateProduct(productId, request);
 
         // then:
-        Assertions.assertThat(product.getName()).isEqualTo("상품 수정");
-        Assertions.assertThat(product.getPrice()).isEqualTo(2000);
-
-
+        final ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+        final GetProductResponse productResponse = response.getBody();
+        Assertions.assertThat(productResponse.name()).isEqualTo("상품 수정");
+        Assertions.assertThat(productResponse.price()).isEqualTo(2000);
     }
 
-    private static class StubProductPort implements ProductPort {
-
-        public Product getProduct_will_return;
-
-        @Override
-        public void save(final Product product) {
-
-        }
-
-        @Override
-        public Product getProduct(final Long productId) {
-            return getProduct_will_return;
-        }
-    }
 }

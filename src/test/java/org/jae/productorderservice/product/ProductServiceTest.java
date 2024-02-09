3c29767fe082c00329b30c55ab37c1eef9440e61
ahn-sj <jaesa5221@gmail.com>
2024-02-09 19:35:58 +0900

    feat: 3-1 POJO 상품 수정 기능 구현하기 (Mock)

--- src/test/java/org/jae/productorderservice/product/ProductServiceTest.java
+++ src/test/java/org/jae/productorderservice/product/ProductServiceTest.java
@@ -4,19 +4,19 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
-    @Autowired
     private ProductService productService;
-
-    private StubProductPort productPort = new StubProductPort();;
+    private ProductPort productPort;
 
     @BeforeEach
     void setUp() {
+        productPort = Mockito.mock(ProductPort.class);
         productService = new ProductService(productPort);
     }
 
@@ -27,7 +27,7 @@ public class ProductServiceTest {
         // given:
         final Long productId = 1L;
         final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        productPort.getProduct_will_return = product;
+        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         // when:
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
